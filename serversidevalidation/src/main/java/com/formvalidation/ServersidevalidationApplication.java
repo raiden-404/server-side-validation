05588a582d459ec6f1139a2f9c718c71a73600d6
raiden-404 <patelyuvrajsingh789@gmail.com>
2025-01-29 02:45:51 +0530

    created spring repository

---
+++ serversidevalidation/src/main/java/com/formvalidation/ServersidevalidationApplication.java
@@ -0,0 +1,13 @@
+package com.formvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServersidevalidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServersidevalidationApplication.class, args);
+	}
+
+}
